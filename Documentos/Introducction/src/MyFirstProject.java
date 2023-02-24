5dc4b46752eec442f832dd372e78336a91a0dad7
sandracr98 <92158166+sandracr98@users.noreply.github.com>
2023-02-24 10:38:05 +0100

    Documentos iniciales

---
+++ Documentos/Introducction/src/MyFirstProject.java
@@ -0,0 +1,6 @@
+public class MyFirstProject {
+    public static void main(String[] args) {
+        System.out.println("Hola a todos");
+    }
+
+}
---
+++ Documentos/Introducction/src/YoSoy.java
@@ -0,0 +1,8 @@
+public class YoSoy {
+    public static void main(String[] args) {
+        // imprime mañana esto jaja salu2
+        System.out.println("Sandra Checa Ruiz");
+        System.out.println("Curso de Java básico");
+    }
+}
+//TODO LOQUESEA
